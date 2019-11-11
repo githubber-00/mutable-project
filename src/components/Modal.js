81b628edbd87793be01a34fe9c1059494689ebec
jonel <gadiajonel@gmail.com>
2019-11-10 17:16:53 -0800

    Closing the modal.

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -12,20 +12,45 @@ export class Modal extends React.Component {
     showModal = () => {
         this.setState({
             isShown: true
+        }, () => {
+            this.closeButton.focus();
         });
-    }
+    };
 
     closeModal = () => {
         this.setState({
             isShown: false
         });
-    }
+    };
+
+    onKeyDown = (event) => {
+        // if the user presses 'Esc' button
+        if (event.keyCode === 27) {
+            this.closeModal();
+        }
+    };
+
+    onClickOutside = (event) => {
+        if (this.modal && this.modal.contains(event.target)) return;
+        this.closeModal();
+    };
 
     render() {
         return (
             <React.Fragment>
                 <ModalTrigger triggerText={this.props.modalProps.triggerText} showModal={this.showModal} />
-                {this.state.isShown ? <ModalContent closeModal={this.closeModal} content={this.props.modalContent} /> : null}
+                {
+                    this.state.isShown ? 
+                        <ModalContent
+                            closeModal={this.closeModal}
+                            content={this.props.modalContent}
+                            onKeyDown={this.onKeyDown}
+                            onClickOutside={this.onClickOutside}
+                            modalRef={n => (this.modal = n)}
+                            buttonRef={n => (this.closeButton = n)}
+                        />
+                    : null
+                }
             </React.Fragment>
         );
     }
--- src/components/ModalContent.js
+++ src/components/ModalContent.js
@@ -5,9 +5,9 @@ import ReactDOM from 'react-dom';
 export class ModalContent extends React.Component {
     render() {
         return ReactDOM.createPortal(
-            <aside className='modal-cover' aria-modal='true' tabIndex='-1' role='dialog'>
-                <div className='modal-area'>
-                    <button className='_modal-close' onClick={this.props.closeModal} aria-label='Close Modal' aria-labelledby='close-modal'>
+            <aside className='modal-cover' aria-modal='true' tabIndex='-1' role='dialog' onKeyDown={this.props.onKeyDown} onClick={this.props.onClickOutside}>
+                <div className='modal-area' ref={this.props.modalRef}>
+                    <button className='_modal-close' ref={this.props.buttonRef} onClick={this.props.closeModal} aria-label='Close Modal' aria-labelledby='close-modal'>
                         <span id='close-modal' className='_hide-visual'>Close</span>
                         <svg className="_modal-close-icon" viewBox="0 0 40 40">
                             <path d="M 10,10 L 30,30 M 30,10 L 10,30" />
