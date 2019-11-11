b09d7893b5eefd7867993e9bc9abff0edbdd88a5
jonel <gadiajonel@gmail.com>
2019-11-10 17:56:54 -0800

    Empty project template.

---
+++ src/components/Modal.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+import ModalTrigger from './ModalTrigger';
+
+export class Modal extends React.Component {
+    render() {
+        return <ModalTrigger />;
+    }
+}
+
+export default Modal;
---
+++ src/components/ModalTrigger.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+export class ModalTrigger extends React.Component {
+    render() {
+        return (
+            <button className='modal-button'>Launch the Modal!</button>
+        );
+    }
+}
+
+export default ModalTrigger;
