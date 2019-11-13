1f2b199602f4b690c3f637ab9ec94252b9bd0cfa
jonel <gadiajonel@gmail.com>
2019-11-12 20:24:25 -0800

    Basic context.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import BookList from './components/BookList';
+import ThemeContextProvider from './context/ThemeContext';
 
 function App() {
     return (
         <div className="App">
+            <ThemeContextProvider>
+                <Navbar />
+                <BookList />
+            </ThemeContextProvider>
         </div>
     );
 }
